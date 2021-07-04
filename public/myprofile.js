688b02af88c30719762113dcb006afae9619c358
Ankit Bajpai <ankit65bajpai@gmail.com>
2021-07-05 00:30:40 +0530

    Modify profile page

--- public/myprofile.js
+++ public/myprofile.js
@@ -1,75 +1,110 @@
 console.log('added');
 
 function openNav() {
-    document.querySelector(".sidebar").style.width = "250px";
-    document.querySelector("#close").style.display = "block";
+  document.querySelector(".sidebar").style.width = "250px";
+  document.querySelector("#close").style.display = "block";
 
-    document.querySelector(".content").style.marginLeft = "24rem";
-    // document.querySelector("#moreInfo").style.position = 'static';
+  document.querySelector(".content").style.marginLeft = "24rem";
+  // document.querySelector("#moreInfo").style.position = 'static';
 
-    // document.querySelector('.table-2').style.display = 'none'
-    // document.getElementById('pinChange').style.display = 'inline-block';
+  // document.querySelector('.table-2').style.display = 'none'
+  // document.getElementById('pinChange').style.display = 'inline-block';
 }
 function closeNav() {
-    document.querySelector(".sidebar").style.width = "0";
-    document.querySelector(".content").style.marginLeft = "";
-    // document.querySelector("#moreInfo").style.position = '';
+  document.querySelector(".sidebar").style.width = "0";
+  document.querySelector(".content").style.marginLeft = "";
+  // document.querySelector("#moreInfo").style.position = '';
 
-    // document.querySelector('.table-2').style.display = 'block'
-    // document.getElementById('pinChange').style.display = 'none';
-    //   if(document.querySelector("#moreInfo").style.position == 'absolute'){
-    //     document.querySelector(".secLable").style.marginRight = '3rem';
-    //   }
-    //   else{
-    //     document.querySelector(".secLable").style.marginRight = '';
-    //   }
+  // document.querySelector('.table-2').style.display = 'block'
+  // document.getElementById('pinChange').style.display = 'none';
+  //   if(document.querySelector("#moreInfo").style.position == 'absolute'){
+  //     document.querySelector(".secLable").style.marginRight = '3rem';
+  //   }
+  //   else{
+  //     document.querySelector(".secLable").style.marginRight = '';
+  //   }
 }
 
-let pin = document.getElementById('pinChange');
-pin.onclick = () => {
-    let table = document.querySelector('.table-2');
-    console.log('btn clicked');
-    table.classList.toggle("clickedPinChangeBtn");
+// let pin = document.getElementById('pinChange');
+// pin.onclick = () => {
+//     let table = document.querySelector('.table-2');
+//     console.log('btn clicked');
+//     table.classList.toggle("clickedPinChangeBtn");
 
-    if (table.classList.contains("clickedPinChangeBtn")){
-        pin.style.backgroundColor = 'black';
-        pin.innerHTML='Add more';
-        document.getElementById('moreInfo').style.display='none';
-        // document.querySelector('.content').style.marginTop='1rem';
-    }
-    else{
-        pin.innerHTML='Change Password';
-        pin.style.backgroundColor = '';
-        document.getElementById('moreInfo').style.display='';
+//     if (table.classList.contains("clickedPinChangeBtn")){
+//         pin.style.backgroundColor = 'black';
+//         pin.innerHTML='Add more';
+//         document.getElementById('moreInfo').style.display='none';
+//         // document.querySelector('.content').style.marginTop='1rem';
+//     }
+//     else{
+//         pin.innerHTML='Change Password';
+//         pin.style.backgroundColor = '';
+//         document.getElementById('moreInfo').style.display='';
 
-        // document.querySelector('.content').style.marginTop='';
-    }
-}
+//         // document.querySelector('.content').style.marginTop='';
+//     }
+// }
 // let edit = document.getElementById('editBtn');
 // edit.addEventListener('click', editInfo);
 
-function editInfo() {
-    let edit = document.getElementById("editBtn");
-    if (edit.innerHTML == "Edit") {
-      alert("Now, you can the Change the user info")
-        edit.innerHTML = "Save";
+document.querySelector('#followerBtn').onclick = () => {
+  let follower = document.querySelectorAll('.follower');
+  // console.log('foll click');
+  for (let i = 0; i < follower.length; i++) {
+    follower[i].classList.toggle('followers');
+    if (follower[i].classList.contains('followers')) {
+      // console.log('present');
+      follower[i].style.display = 'block';
+      document.querySelector('#followerBtn').style.textDecoration='underline';
     }
-    else edit.innerHTML = "Edit";
+    else {
+      //  console.log('absent');
+      follower[i].style.display = '';
+      document.querySelector('#followerBtn').style.textDecoration='';
+    }
+  }
 }
-var i=0;
-function saveedit(){
-  if(i%2==0){
+document.querySelector('#followingBtn').onclick = () => {
+  let following = document.querySelectorAll('.following');
+  // console.log('foll click');
+  for (let i = 0; i < following.length; i++) {
+    following[i].classList.toggle('followings');
+    if (following[i].classList.contains('followings')) {
+      // console.log('present');
+      following[i].style.display = 'block';
+      document.querySelector('#followingBtn').style.textDecoration='underline';
+    }
+    else {
+      //  console.log('absent');
+      following[i].style.display = '';
+      document.querySelector('#followingBtn').style.textDecoration='';
+    }
+  }
+}
+
+function editInfo() {
+  let edit = document.getElementById("editBtn");
+  if (edit.innerHTML == "Edit") {
+    alert("Now, you can the Change the user info")
+    edit.innerHTML = "Save";
+  }
+  else edit.innerHTML = "Edit";
+}
+var i = 0;
+function saveedit() {
+  if (i % 2 == 0) {
     $("#FName").prop("readonly", false);
     $("#LName").prop("readonly", false);
     editInfo();
-  }else{
+  } else {
     let data = {
-      FName : $('#FName').val(),
+      FName: $('#FName').val(),
       LName: $('#LName').val(),
     }
     // console.log(data);
     data = JSON.stringify(data);
-    $.post('/addmoredetails', { data: data }, function(data){
+    $.post('/addmoredetails', { data: data }, function (data) {
       console.log(data);
     });
     $("#FName").prop("readonly", true);
@@ -79,12 +114,12 @@ function saveedit(){
   i++;
 }
 
-function savedetail(){
+function savedetail() {
   // console.log("yes");
   let data = {
-    Description : $("#Description").val(),
-    Education : $('#edu').val(),
-    State : $('#state').val(),
+    Description: $("#Description").val(),
+    Education: $('#edu').val(),
+    State: $('#state').val(),
     City: $('#city').val()
   }
   data = JSON.stringify(data);
