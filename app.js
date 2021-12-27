ba3cf6d1ee73f339b120f8783a75f9aa71b2bdae
tushar gupta <tushargupta25860@gmail.com>
2021-12-27 21:14:07 +0530

    port address

--- app.js
+++ app.js
@@ -1015,6 +1015,6 @@ app.get("/", function (req, res) {
 
 
 
-app.listen(3001, function (req, res) {
-  console.log("up and running");
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
