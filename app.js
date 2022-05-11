61958e2233ad90c2d18bb3e8fe5c4527238a16cd
tushar gupta <tushargupta25860@gmail.com>
2022-05-11 16:44:50 +0530

    corrected2

--- app.js
+++ app.js
@@ -1012,9 +1012,9 @@ app.get("/", function (req, res) {
   }
 });
 
-app.listen(process.env.PORT || 3000, function () {
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-});
+// app.listen(process.env.PORT || 3000, function () {
+//   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+// });
 let port = process.env.PORT;
 if (port == null || port == "") {
   port = 3001;
